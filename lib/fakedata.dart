3edd1c80ddb697c835edb1ebf7fa648bad9944f6
Sergey Todyshev <stodyshev@gmail.com>
2018-12-25 05:54:05 +0000

    new words

--- lib/fakedata.dart
+++ lib/fakedata.dart
@@ -28,6 +28,46 @@ var words = [
     'pronunciation@en': 'https://howjsay.com/mp3/girl.mp3',
     'image': 'https://i.ytimg.com/vi/ktlQrO2Sifg/maxresdefault.jpg',
   },
+  {
+    'text@en': 'bed',
+    'text@ru': 'кровать',
+    'transcription@en': 'bed',
+    'transcription@ru': "'бэд",
+    'pronunciation@en': 'https://howjsay.com/mp3/bed.mp3',
+    'image': 'https://hoff.ru//upload/iblock/9be/9be0921f96cf2f30b4e5136c4eccd7d8.jpg',
+  },
+   {
+    'text@en': 'boy',
+    'text@ru': 'мальчик',
+    'transcription@en': 'bOI',
+    'transcription@ru': "'бой",
+    'pronunciation@en': 'https://howjsay.com/mp3/boy.mp3',
+    'image': 'https://www.paparazzi.ru/upload/wysiwyg_files/img/1472483485.jpg',
+  },
+  {
+    'text@en': 'body',
+    'text@ru': 'тело',
+    'transcription@en': 'bOdI',
+    'transcription@ru': "'боди",
+    'pronunciation@en': 'https://howjsay.com/mp3/body.mp3',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKLUYuJGon1owJX__Y7Ov3uzLVPwMzK7dXDS375I5DRRP3ISprGA',
+  },
+   {
+    'text@en': 'car',
+    'text@ru': 'автомобиль',
+    'transcription@en': 'kɑːr',
+    'transcription@ru': "'кар",
+    'pronunciation@en': 'https://howjsay.com/mp3/car.mp3',
+    'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmIGgC7NLbTyrby1pTLhexemP0apw-H_zJ0at2BdJhPuHdhqiu',
+  },
+  {
+    'text@en': 'door',
+    'text@ru': 'дверь',
+    'transcription@en': 'dɔːr',
+    'transcription@ru': "'дор",
+    'pronunciation@en': 'https://howjsay.com/mp3/door.mp3',
+    'image': 'https://images.obi.ru/product/RU/800x600/400211_1.jpg',
+  },
 ].map((t) => Word.fromJson(t))
 .toList();
 
