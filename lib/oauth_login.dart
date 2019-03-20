661e85d77c367be080d3e78399121a5b725d49cd
sergeyt <stodyshev@gmail.com>
2019-03-21 06:56:21 +0700

    try using mobile user agent to improve layout of login pages

--- lib/oauth_login.dart
+++ lib/oauth_login.dart
@@ -12,7 +12,7 @@ oauthLogin(BuildContext ctx, String provider) async {
     url,
     withJavascript: true,
     appCacheEnabled: true,
-    userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    userAgent: 'Mozilla/5.0 (Linux; Android 8.0.0; TA-1053 Build/OPR1.170623.026) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3368.0 Mobile Safari/537.36',
   );
   var onToken = new StreamController();
 
